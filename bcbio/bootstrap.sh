470bc9cf2b45d5e4e130ac7af55fd2ed90b0f0f6
roryk <rory.kirchner@gmail.com>
2013-10-18 01:17:43 -0400

    bcbio development vm

---
+++ bcbio/bootstrap.sh
@@ -0,0 +1,12 @@
+#!/usr/bash
+sudo apt-get install leiningen
+sudo apt-get update
+sudo apt-get install -y build-essential
+sudo apt-get install -y git
+sudo apt-get install -y libz-dev
+mkdir tmp
+cd tmp
+wget http://09c8d0b2229f813c1b93-c95ac804525aac4b6dba79b00b39d1d3.r79.cf1.rackcdn.com/Anaconda-1.7.0-Linux-x86_64.sh
+#mkdir tmp
+#cd tmp
+#wget https://raw.github.com/chapmanb/bcbio-nextgen/master/scripts/bcbio_nextgen_install.py
